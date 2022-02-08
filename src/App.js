b2c7d8a1eed11a32b590bc1dd9984db7f02537be
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-08 23:07:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className='App'>
       <div className='container'>
-        <h1 className='text-center'>Request your original diamond right now!</h1>
+        <h1 className='text-center'>Impact Gemstone</h1>
         <ContactForm />
       </div>
     </div>
